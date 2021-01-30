7f7a0ab290a268e01cb8c362570a85e1ce77d0a7
James Dougherty <jjdougherty89@gmail.com>
2021-01-30 02:08:17 -0500

    Initial commit

---
+++ src/test/java/com/doughdesign/sbcontactapi/SbContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doughdesign.sbcontactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbContactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
