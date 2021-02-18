b981c88ecb7910fcd62c48455f87c96920679efd
James Dougherty <jjdougherty89@gmail.com>
2021-02-17 20:12:39 -0500

    Add CORS annotation

--- src/main/java/com/doughdesign/sbcontactapi/ContactController.java
+++ src/main/java/com/doughdesign/sbcontactapi/ContactController.java
@@ -1,12 +1,14 @@
 package com.doughdesign.sbcontactapi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ContactController {
 
+    @CrossOrigin
     @GetMapping("/contacts")
     @ResponseBody
     String getContacts() {
